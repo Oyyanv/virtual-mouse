b53dedf46c16ed4378d5b923fbd8cd5109753f09
Oyyanv <muhammadroyyan527@gmail.com>
2025-11-07 15:26:24 +0700

    refactor camera settings and improve hand tracking logic

--- cam.py
+++ cam.py
@@ -6,10 +6,16 @@ import math
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
-hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7)
-
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=1,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+    model_complexity=0)
 
 cam = cv2.VideoCapture(0)
+cam.set(3, 1200) # width
+cam.set(4, 900)  # height
 
 #gesture time
 click_start_time = None
@@ -28,8 +34,10 @@ if not cam.isOpened():
     exit()
 
 previous_time = 0
+frame_count = 0
 
 while True:
+
     ret, frame = cam.read()
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
@@ -46,28 +54,25 @@ while True:
         for hand_landmarks in results.multi_hand_landmarks:
              mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
     
-        cv2.putText(frame, f'FPS: {int(fps)}', (10, 30),
-                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         #finger tip
         thumb_tip = hand_landmarks.landmark[4]
         index_tip = hand_landmarks.landmark[8]
         middle_tip = hand_landmarks.landmark[12]
-        ring_tip = hand_landmarks.landmark[16]
-        pinky_tip = hand_landmarks.landmark[20]
-    
+        # ring_tip = hand_landmarks.landmark[16]
+        # pinky_tip = hand_landmarks.landmark[20]
         finger = [
             1 if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[tip - 2].y else 0
-            for tip in [4, 8, 12, 16, 20]
+            for tip in [4, 8, 12]
         ]
     
         #distance index and thumb
         distance = math.hypot(thumb_tip.x - index_tip.x, thumb_tip.y - index_tip.y)
-        if distance < 0.04:
+        if distance < 0.06:
             if not freeze_mode:
                 freeze_mode = True
                 click_time.append(time.time())
             
-            #2x click
+            #2x click and 1x click
             if len(click_time) >= 2 and click_time[-1] - click_time[-2]<0.4:
                 pyautogui.doubleClick()
                 cv2.putText(frame, 'Double Click', (10, 50),
@@ -79,8 +84,17 @@ while True:
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         else:
             freeze_mode = False
-                
 
+        #moving cursor with index finger
+        if not freeze_mode:
+            screen_x = int(index_tip.x * screen_w)
+            screen_y = int(index_tip.y * screen_h)
+            pyautogui.moveTo(screen_x, screen_y, duration=0)
+            previous_screen_x, previous_screen_y = screen_x, screen_y
+
+    cv2.putText(frame, f'FPS: {int(fps)}', (10, 30),
+        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        
     cv2.imshow('Live Cam', frame) 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
